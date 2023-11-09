6397e9eefdcba46701455e7edf54994a20949f5c
dev-tae <taehyun.kim0913@gmail.com>
2023-11-09 13:51:57 -0500

    Utilize lombok and make a progress in model and service layers

--- src/main/java/io/github/devtae/taskmanagementsystem/model/Task.java
+++ src/main/java/io/github/devtae/taskmanagementsystem/model/Task.java
@@ -1,10 +1,19 @@
 package io.github.devtae.taskmanagementsystem.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "tasks")
 public class Task {
 
@@ -12,9 +21,12 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 100, message = "Title cannot be longer than 100 characters")
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Size(max = 500, message = "Description cannot be longer than 500 characters")
     @Column(name = "description")
     private String description;
 
@@ -24,47 +36,35 @@ public class Task {
     @Column(name = "completed")
     private boolean isCompleted;
 
-    public Long getId() {
-        return id;
-    }
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-    public String getTitle() {
-        return title;
-    }
+    /**
+     * Checks if the current task entity has updates compared to the details provided.
+     * Compares editable fields of the task to see if any changes were made.
+     *
+     * @param taskDetails the task details to compare against.
+     * @return true if there are changes, false otherwise.
+     */
+    public boolean hasUpdates(Task taskDetails) {
+        // Check title for changes, as it should not be null or empty
+        if (!Objects.equals(this.title, taskDetails.getTitle())) return true;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+        // Check description for changes. It could be an update from non-empty to empty.
+        if (!Objects.equals(this.description, taskDetails.getDescription())) return true;
 
-    public String getDescription() {
-        return description;
-    }
+        // Check dueDate for changes. The date might have been adjusted.
+        if (!Objects.equals(this.dueDate, taskDetails.getDueDate())) return true;
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+        // Check completed status for changes, which can be toggled by the user.
+        if (this.isCompleted() != taskDetails.isCompleted()) return true;
 
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-
-    public void setCompleted(boolean completed) {
-        isCompleted = completed;
-    }
-
-    public Task() {
-        // Default constructor
+        // If none of the fields have been changed, return false.
+        return false;
     }
 }
