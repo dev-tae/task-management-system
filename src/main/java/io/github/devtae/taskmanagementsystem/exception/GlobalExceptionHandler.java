2e2f60900bbb54440f02de8d88afd770e6531713
dev-tae <taehyun.kim0913@gmail.com>
2023-11-10 13:44:56 -0500

    Implement Global Exception Handler and make structure modifications

---
+++ src/main/java/io/github/devtae/taskmanagementsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package io.github.devtae.taskmanagementsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException ex, WebRequest request) {
+        // Create a structured error response body
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Task not found");
+
+        // Return the response entity with a specific HTTP status
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        // Collect all the errors
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getField() + ": " + x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    // Generic exception handler as a catch-all
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "An error occurred");
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
--- src/main/java/io/github/devtae/taskmanagementsystem/model/Task.java
+++ src/main/java/io/github/devtae/taskmanagementsystem/model/Task.java
@@ -9,7 +9,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Data
@@ -43,28 +42,4 @@ public class Task {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    /**
-     * Checks if the current task entity has updates compared to the details provided.
-     * Compares editable fields of the task to see if any changes were made.
-     *
-     * @param taskDetails the task details to compare against.
-     * @return true if there are changes, false otherwise.
-     */
-    public boolean hasUpdates(Task taskDetails) {
-        // Check title for changes, as it should not be null or empty
-        if (!Objects.equals(this.title, taskDetails.getTitle())) return true;
-
-        // Check description for changes. It could be an update from non-empty to empty.
-        if (!Objects.equals(this.description, taskDetails.getDescription())) return true;
-
-        // Check dueDate for changes. The date might have been adjusted.
-        if (!Objects.equals(this.dueDate, taskDetails.getDueDate())) return true;
-
-        // Check completed status for changes, which can be toggled by the user.
-        if (this.isCompleted() != taskDetails.isCompleted()) return true;
-
-        // If none of the fields have been changed, return false.
-        return false;
-    }
 }
